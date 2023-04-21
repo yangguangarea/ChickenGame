1f6b019739d8e4dcb01d07a8efe91874141733e7
yangguangarea <594458046@qq.com>
2023-04-21 15:48:01 +0800

    抽奖旋转逻辑

--- assets/script/TableDialog.ts
+++ assets/script/TableDialog.ts
@@ -12,6 +12,8 @@ export default class TableDialog extends cc.Component {
     lightNode: cc.Node = null;
     
 
+    isStartReward: boolean = false;//是否处于抽奖
+
     start () {
         let content = this.node.getChildByName('content');
         content.scale = 0;
@@ -85,15 +87,27 @@ export default class TableDialog extends cc.Component {
         //先打开市民贷10秒倒计时弹窗
         //关闭弹窗，转盘开始转
 
-
         //默认逆时针旋转
 
         // 360/16
+        let reward = '16.88';
+        let rewardMap = [1.68, 8.88, 2.68, 5.88, '谢谢参与', 16.88, 1.68, 2.68]
 
-        // 22.5 + 45 * n
-        // this.tableNode.rotation
-        //停止转动
-        //弹出中奖金额弹窗
+        let rotate = 0;
+        for (let i = 0; i < rewardMap.length; i++) {
+            if(`${rewardMap[i]}` === `${reward}`) {
+                // rotate = 22.5 + 45 * i + 360 * 2;
+                rotate = -22.5 - 45 * i - 360 * 2;
+                console.log("-----旋转的是", i, rewardMap[i]);
+            }
+        }
+        
+        console.log("----rotate", rotate);
+        this.isStartReward = true;
+        this.tableNode.runAction(cc.sequence(cc.rotateBy(2, rotate).easing(cc.easeCubicActionOut()), cc.delayTime(0.5), cc.callFunc(()=> {
+            //弹出中奖金额弹窗
+            this.isStartReward = false;
+        })));
     }
 
     //了解贷款
